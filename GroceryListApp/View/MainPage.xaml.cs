1b53d28da3d49011296bbef6e6a0b7a9da5c6eed
Eric Klomp <Eric.Klomp.0273@waltersgardens.com>
2026-07-17 17:01:14 -0400

    adding entry box and add button

---
+++ GroceryListApp/View/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace GroceryListApp
+{
+  public partial class MainPage : ContentPage
+  {
+
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private void AddItemButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+   }
+}
